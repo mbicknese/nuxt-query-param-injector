17f754a43fe3b1aa29e79345c79742668d7e44e9
Maarten Bicknese <maarten.bicknese@gmail.com>
2019-07-02 14:04:08 +0200

    Setup basic PoC

---
+++ components/WithQueryParam.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: {
+    queryKey: {
+      type: String,
+      required: true
+    }
+  },
+  render() {
+    return this.$scopedSlots.default(this.$route.query[this.queryKey])
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,28 +8,30 @@
       <h2 class="subtitle">
         Proof of Concept for injecting query params
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <WithQueryParam query-key="foo">
+        <template #default="param">
+          <div>
+            <NameGreeter :name="param" />
+            <p v-if="!param" class="links">
+              Please navigate to http://localhost:3000/?foo=[yourname]
+            </p>
+          </div>
+        </template>
+      </WithQueryParam>
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import NameGreeter from '~/components/NameGreeter.vue'
+import WithQueryParam from '~/components/WithQueryParam.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    NameGreeter,
+    WithQueryParam
   }
 }
 </script>
